cde9347ee6a4bacd5ac68178837f6d3ab97f253c
Michael Lang <michael.langh-da.de>
2022-10-14 22:02:24 +0200

    ext tables

--- ext_tables.php
+++ ext_tables.php
@@ -3,7 +3,6 @@ defined('TYPO3_MODE') or die();
 
 
 $boot = function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3up_slideshow', 'Configuration/TypoScript', 'T3UP - Slideshow');
 	$GLOBALS['TCA']['tt_content']['columns']['pi_flexform']['config']['ds'][',slideshow'] = 'FILE:EXT:t3up_slideshow/Configuration/FlexForms/Slideshow.xml';
 };
 
