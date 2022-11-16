7fdd714a1944c9952428a6873f97babc8ef3669a
Michael Lang <michael.langh-da.de>
2022-11-16 06:59:48 +0100

    Version 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'T3UP - Slideshow',
     'description'      => 'Bootstrap slideshows for Images',
-    'version'          => '1.0.0',
+    'version'          => '10.0.0',
     'state'            => 'stable',
     'category'         => 'fe',
     'author'           => 'Michael Lang',
