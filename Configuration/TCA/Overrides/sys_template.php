0201af57d678b1ca8695ea4525be4a7cb5eca173
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-02-28 06:27:54 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,5 +1,4 @@
 <?php
-
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die;
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('t3up_slideshow', 'Configuration/TypoScript', 'T3UP - Slideshow-Installation');
