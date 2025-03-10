cb3a2a30f77315ca682590b9a93133b2a1f6b43e
Michael Lang <113889180+t3ac@users.noreply.github.com>
2025-03-10 18:15:04 +0100

    Update slideshow.php

--- Configuration/TCA/Overrides/slideshow.php
+++ Configuration/TCA/Overrides/slideshow.php
@@ -29,7 +29,7 @@ call_user_func(function () {
             ' . $frontendLanguageFilePrefix . 'palette.general;general,
              header,
              assets,
-	    --div--;LLL:EXT:t3up_headerimage/Resources/Private/Language/locallang_backend.xlf:headerimage.flexvalues, pi_flexform,
+	    --div--;LLL:EXT:t3up_slideshow/Resources/Private/Language/locallang_backend.xlf:headerimage.flexvalues, pi_flexform,
 	    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
 	    --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.appearanceLinks;appearanceLinks,space_after_class,
 	    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,--palette--;;language,
